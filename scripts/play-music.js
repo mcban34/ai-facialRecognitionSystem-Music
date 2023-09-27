d772f9e560c3c09c4e2fa2659803f5db0e342ed7
mehmet çoban <ccobanmehmet@gmail.com>
2023-09-27 18:24:49 +0300

    music play ok

--- scripts/play-music.js
+++ scripts/play-music.js
@@ -5,52 +5,82 @@ const currentTimeDisplay = document.getElementById('currentTime');
 
 let isPlaying = false;
 
-// Çal/Duraklat işlevini değiştir
-playPauseButton.addEventListener('click', () => {
-    if (!isPlaying) {
-        audioElement.play();
-        playPauseButton.textContent = 'Duraklat';
-    } else {
-        audioElement.pause();
-        playPauseButton.textContent = 'Çal';
-    }
-    isPlaying = !isPlaying;
-});
-
-// İlerleme çubuğunu güncelle
-audioElement.addEventListener('timeupdate', () => {
-    const currentTime = audioElement.currentTime;
-    const duration = audioElement.duration;
-
-    // İlerleme çubuğunu güncelle
-    seekBar.value = currentTime;
-    seekBar.max = duration;
-
-    // Geçen süreyi güncelle
-    currentTimeDisplay.textContent = formatTime(currentTime);
-});
-
-// İlerleme çubuğunu sürükleme
-seekBar.addEventListener('input', () => {
-    audioElement.currentTime = seekBar.value;
-});
-
-// Süre bilgisini dakika:saniye formatına çevirme
-function formatTime(seconds) {
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = Math.floor(seconds % 60);
-    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
-}
-
 document.addEventListener("DOMContentLoaded", () => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const situation = urlParams.get('situation');
     console.log(situation);
     fetch("data/data.json")
-    .then(res => res.json())
-    .then(value => {
-        const musicFilter = value.filter(elemet => elemet.situation==situation) 
-        audioElement.src=musicFilter[0].path
-    })
-})
+        .then(res => res.json())
+        .then(value => {
+            const musicList = value.filter(element => element.situation == situation);
+
+            let currentPlayingButton = null;
+
+            musicList.forEach(music => {
+                const audioElement = document.createElement("audio");
+                audioElement.src = music.path;
+
+                let isPlaying = false;
+                const playButton = document.createElement("button");
+                playButton.textContent = "Çal";
+
+                const artTitle = document.createElement("h3")
+                artTitle.innerHTML = music.name
+
+                playButton.addEventListener("click", () => {
+                    if (!isPlaying) {
+                        audioElement.currentTime = 0;
+                        seekBar.value = 0;
+
+                        const allAudioElements = document.querySelectorAll('audio');
+                        allAudioElements.forEach(element => {
+                            if (element !== audioElement) {
+                                element.pause();
+                            }
+                        });
+
+                        if (currentPlayingButton) {
+                            currentPlayingButton.textContent = 'Çal';
+                        }
+
+                        audioElement.play();
+                        playButton.textContent = 'Duraklat';
+                        currentPlayingButton = playButton;
+                    } else {
+                        audioElement.pause();
+                        playButton.textContent = 'Çal';
+                    }
+                    isPlaying = !isPlaying;
+                });
+
+                audioElement.addEventListener('timeupdate', () => {
+                    const currentTime = audioElement.currentTime;
+                    const duration = audioElement.duration;
+
+                    seekBar.value = currentTime;
+                    seekBar.max = duration;
+
+                    currentTimeDisplay.textContent = formatTime(currentTime);
+                });
+
+                seekBar.addEventListener('input', () => {
+                    audioElement.currentTime = seekBar.value;
+                });
+
+                document.body.appendChild(audioElement);
+                document.body.appendChild(playButton);
+                document.body.appendChild(seekBar);
+                document.body.appendChild(currentTimeDisplay);
+                document.body.appendChild(artTitle)
+            });
+        });
+});
+
+function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+}
+
+
